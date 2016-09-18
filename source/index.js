f04c43e09cf2670744778a224c402d62dbee9756
Mario Nebl <hello@herebecode.com>
2016-09-18 11:13:54 +0200

    feat: recursively transform dependencies

--- source/index.js
+++ source/index.js
@@ -4,8 +4,29 @@ export default function createBabelTransform() {
 	return babelTransform;
 }
 
-async function babelTransform(file, _, configuration) {
+function babelTransform(file, _, configuration) {
 	const source = typeof file.buffer === 'string' ? file.buffer : file.buffer.toString('utf-8');
-	file.buffer = await transform(source, configuration.opts).code;
+	file.buffer = transform(source, configuration.opts).code;
+
+	if (configuration.recursive) {
+		walk(file.dependencies, dependency => {
+			const dependencySource = typeof dependency.buffer === 'string' ? dependency.buffer : dependency.buffer.toString('utf-8');
+			dependency.buffer = transform(dependencySource, configuration.opts).code;
+		});
+	}
+
 	return file;
+}
+
+function walk(dependencyTree, predecate) {
+	flatten(dependencyTree).forEach(predecate);
+}
+
+function flatten(dependencyTree, vault = []) {
+	return Object.values(dependencyTree || {})
+		.reduce((list, item) => {
+			list.push(item);
+			flatten(item.dependency, list);
+			return list;
+		}, vault);
 }
