967df8e1f76ae51950b7696836333bb69fbd899a
Mario Nebl <hello@herebecode.com>
2016-09-18 10:41:54 +0200

    test: add smoke tests

--- source/index.js
+++ source/index.js
@@ -1,15 +1,17 @@
 import {transform} from 'babel-core';
 
 export default function createBabelTransform() {
-	return async function babelTransform(file, demo, configuration) {
-		const source = typeof file.buffer === 'string' ? file.buffer : file.buffer.toString('utf-8');
-		file.buffer = transform(source, configuration.opts).code;
+	return babelTransform;
+}
 
-		if (demo) {
-			const demoSource = typeof demo.buffer === 'string' ? demo.buffer : demo.buffer.toString('utf-8');
-			demo.buffer = transform(demoSource, configuration.opts).code;
-		}
+async function babelTransform(file, demo, configuration) {
+	const source = typeof file.buffer === 'string' ? file.buffer : file.buffer.toString('utf-8');
+	file.buffer = await transform(source, configuration.opts).code;
 
-		return file;
-	};
+	if (demo) {
+		const demoSource = typeof demo.buffer === 'string' ? demo.buffer : demo.buffer.toString('utf-8');
+		demo.buffer = await transform(demoSource, configuration.opts).code;
+	}
+
+	return file;
 }
