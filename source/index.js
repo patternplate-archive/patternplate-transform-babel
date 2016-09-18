58137da2890ec7907c89f1e58a6ceaadc60f8b8d
Mario Nebl <hello@herebecode.com>
2016-09-18 10:49:51 +0200

    chore: remove obsolete demo handling

--- source/index.js
+++ source/index.js
@@ -4,14 +4,8 @@ export default function createBabelTransform() {
 	return babelTransform;
 }
 
-async function babelTransform(file, demo, configuration) {
+async function babelTransform(file, _, configuration) {
 	const source = typeof file.buffer === 'string' ? file.buffer : file.buffer.toString('utf-8');
 	file.buffer = await transform(source, configuration.opts).code;
-
-	if (demo) {
-		const demoSource = typeof demo.buffer === 'string' ? demo.buffer : demo.buffer.toString('utf-8');
-		demo.buffer = await transform(demoSource, configuration.opts).code;
-	}
-
 	return file;
 }
